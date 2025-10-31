024fda486ed1f8e8ebbec704fb986765976344eb
Mateusz Krysiak <mateuszkrysiak05@gmail.com>
2025-10-31 01:21:37 +0100

    initial commit

---
+++ src/main/java/com/mcdevka/realestate_projects_tracker/RealestateProjectsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mcdevka.realestate_projects_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateProjectsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateProjectsTrackerApplication.class, args);
+	}
+
+}
