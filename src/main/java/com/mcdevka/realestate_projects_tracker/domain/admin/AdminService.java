c5c7a291d186d86481d8c4946e65e4208758f898
Mateusz <mateuszkrysiak05@gmail.com>
2026-03-18 22:01:05 +0100

    fixed import

--- src/main/java/com/mcdevka/realestate_projects_tracker/domain/admin/AdminService.java
+++ src/main/java/com/mcdevka/realestate_projects_tracker/domain/admin/AdminService.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
